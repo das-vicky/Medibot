d0d7c457b896a023ac4029c05b348401ba226f62
Vicky Das <vickydas3720190@gmail.com>
2025-08-06 09:33:28 +0530

    requiremts added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medical_chatbot",
+    version = "0.1.0",
+    author="Vicky Das",
+    author_email = "vickydas3720190@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
